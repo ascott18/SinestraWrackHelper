9e4d00e8c21d070b8da4e234a29e09e90b6e0b8c
cybeloras <ascott18@gmail.com>
2011-05-24 00:13:06 +0000

    ===v2.0=== (and other releases since v1.0)  * Completely overhauled the config mode so that it tries to to break everything in sight when you toggle it on.
    git-svn-id: https://repos.curseforge.com/wow/sinestra-wrack-helper/trunk@14 6d19088a-aebc-42a6-a98f-87d5678736e7

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -1,44 +1,56 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale("SinestraWrackHelper", "enUS", true, true)
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("SinestraWrackHelper", "enUS", true)
 
-L["The color of the status bar when the bar has reached its max (when wrack should be dispelled)"] = true
-L["Scale"] = true
-L["Right"] = true
-L["Bar Height"] = true
-L["Check to show this font string, uncheck to hide"] = true
-L["Whether the bars should expand upwards or downwards."] = true
-L["Thin Outline"] = true
-L["Maximum Bar value"] = true
-L["Bar Spacing"] = true
-L["Font color"] = true
-L["Font - Time Active"] = true
-L["Side of bar to anchor to"] = true
-L["Bar Width"] = true
-L["Check to show this lock the movement and hide test bars, uncheck to unlock movement and show test bars"] = true
-L["Configue the icon that is displayed to the side of the bar showing various buffs on the bar's unit that affect wrack"] = true
-L["Down"] = true
-L["Font Face"] = true
-L["Font Size"] = true
-L["Font outline style"] = true
-L["Font - Player Name"] = true
-L["X offset"] = true
-L["Y offset"] = true
-L["Locked"] = true
-L["Options"] = true
-L["Icon Options"] = true
-L["Left"] = true
-L["The amount of space between each bar"] = true
-L["Font - Last Damage"] = true
-L["Click and drag bars to reposition. '/swh options' to display the options. '/swh' to leave config mode."] = true
-L["Up"] = true
-L["End color"] = true
-L["The color of the status bar when wrack has just been applied to a player"] = true
-L["Start color"] = true
-L["Bar Options"] = true
-L["Colors"] = true
-L["Growth Direction"] = true
-L["No Outline"] = true
-L["Enabled"] = true
-L["Thick Outline"] = true
-L["Check to show the icons to the side of bars that show various buffs that affect wrack's damage, uncheck to hide"] = true
-L["The time, in seconds, that the bar will be full at. Higher times than this value will continue to show a full bar."] = true
-L["Bar Texture"] = true
+
+L["LOCKED"] = "Locked"
+L["ENABLED"] = "Enabled"
+L["SCALE"] = "Scale"
+L["LEFT"] = "Left"
+L["RIGHT"] = "Right"
+L["UP"] = "Up"
+L["DOWN"] = "Down"
+L["XOFFS"] = "X offset"
+L["YOFFS"] = "Y offset"
+L["OPTIONS"] = "Options"
+L["CONFIG"] = "Config"
+
+L["CONFIG"] = "Config"
+L["CONFIG_LOCK"] = "Check this to show lock the movement and hide test bars, uncheck to unlock movement and show test bars"
+L["CONFIG_TEXT"] = "Click and drag bars to reposition. Type '/swh options' to display the options, and '/swh' to leave config mode."
+
+L["BAR_HEADER"] = "Bar Options"
+L["BAR_HEIGHT"] = "Bar Height"
+L["BAR_WIDTH"] = "Bar Width"
+L["BAR_SPACING"] = "Bar Spacing"
+L["BAR_SPACING_DESC"] = "The amount of space between each bar"
+L["BAR_TEXTURE"] = "Bar Texture"
+L["BAR_DIRECTION"] = "Growth Direction"
+L["BAR_DIRECTION_DESC"] = "Whether the bars should expand upwards or downwards."
+L["BAR_COLORS"] = "Colors/Transparency"
+L["BAR_COLOR_START"] = "Start color"
+L["BAR_COLOR_START_DESC"] = "The color of the status bar when Wrack has just been applied to a player"
+L["BAR_COLOR_END"] = "End color"
+L["BAR_COLOR_END_DESC"] = "The color of the status bar when the bar has reached its max (when Wrack should be dispelled)"
+L["BAR_COLOR_BKG"] = "Background color"
+L["BAR_COLOR_BKG_DESC"] = "The color of the bar's background"
+L["BAX_MAX"] = "Maximum Bar value"
+L["BAR_MAX_DESC"] = "The time, in seconds, that the bar will be full at. Higher times than this value will continue to show a full bar."
+
+L["FONT_HEADER_TIMEACTIVE"] = "Font - Time Active"
+L["FONT_HEADER_PLAYERNAME"] = "Font - Player Name"
+L["FONT_HEADER_LASTDAMAGE"] = "Font - Last Damage"
+L["FONT_ENABLE"] = "Check to show this font string, uncheck to hide"
+L["FONT_TYPEFACE"] = "Font Face"
+L["FONT_SIZE"] = "Font Size"
+L["FONT_OUTLINE"] = "Font outline style"
+L["FONT_COLOR"] = "Font color"
+L["MONOCHROME"] = "No Outline"
+L["OUTLINE"] = "Thin Outline"
+L["THICKOUTLINE"] = "Thick Outline"
+
+L["ICON_HEADER"] = "Icon Options"
+L["ICON_DESC"] = "Configure the icon that is displayed to the side of the bar showing various buffs on the bar's unit that affect Wrack"
+L["ICON_ENABLE_DESC"] = "Check to show the icons to the side of bars that show various buffs that affect Wrack's damage, uncheck to hide"
+L["ICON_ANCHORSIDE"] = "Side of bar to anchor to"
+
+
+
